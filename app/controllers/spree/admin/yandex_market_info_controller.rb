edd46e9af35b1b319bf7336edf7b49de814cad6d
Maxim Kulkin <mkulkin@mirantis.com>
2012-08-09 17:35:01 +0400

    Added filter and alert on properties with v/ long values

--- app/controllers/spree/admin/yandex_market_info_controller.rb
+++ app/controllers/spree/admin/yandex_market_info_controller.rb
@@ -8,6 +8,13 @@ class Spree::Admin::YandexMarketInfoController < Spree::Admin::BaseController
 
   def update
     SpreeYandexMarketScraper::Scraper.scrap_and_update(params[:yandex_market_link], @product)
+
+    property_count = @product.product_properties.size
+    @product.product_properties.reject! { |p| p.value.length > 255 }
+    if property_count != @product.product_properties.size
+      flash[:error] = I18n.t(:'yandex_market.error.long_property_value') 
+    end
+
     if @product.save
       flash.notice = flash_message_for(@product, :successfully_updated)
       redirect_to edit_admin_product_url(@product)
